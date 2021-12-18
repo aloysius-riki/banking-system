a7b1044507859621491e569161cdcbb61bc1cdcf
Zjukamu Riki <68075271+zjukamu-riki@users.noreply.github.com>
2021-12-18 16:02:25 +0300

    Added generation and display of random card and pin number

--- src/com/zjukamuriki/Main.java
+++ src/com/zjukamuriki/Main.java
@@ -1,5 +1,7 @@
 package com.zjukamuriki;
 
+import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
@@ -20,6 +22,7 @@ public class Main {
         System.out.println("1. Create an account");
         System.out.println("2. Log into account");
         System.out.println("0. Exit");
+
         int menuItem = scanner.nextInt();
 
         if (menuItem == 1) {
@@ -38,11 +41,39 @@ public class Main {
 
     public static void createAccount() {
 
+        Random random = new Random();
+        int randomAccountId = random.nextInt(1000000000);
+        int randomChecksum = random.nextInt(10);
+        int randomCardPin = random.nextInt(10000);
+        final int cardLength = 16;
+
+        long[] cardId = new long[cardLength];
+        cardId [0] = 4;
+        cardId [cardLength - 1] = randomChecksum;
+
+        String tempAccountId = Integer.toString(randomAccountId);
+        for (int i = 6; i < cardLength - 1 ; i++) {
+            cardId[i] = tempAccountId.charAt(i - 6) - '0';
+        }
+
+        String tempCardPin = Integer.toString(randomCardPin);
+        int[] cardPin = new int[tempCardPin.length()];
+        for(int j = 0; j < tempCardPin.length(); j++) {
+            cardPin[j] = tempCardPin.charAt(j) - '0';
+        }
+
         System.out.println("Your card has been created");
         System.out.println("Your card number:");
-        System.out.println("4000004938320895");
+        for (long cardIdValue : cardId) {
+            System.out.print(cardIdValue);
+        }
+        System.out.println();
         System.out.println("Your card PIN:");
-        System.out.println("1234");
+        for (long cardPinValue : cardPin) {
+            System.out.print(cardPinValue);
+        }
+        System.out.println();
+
 
         homePage();
     }
